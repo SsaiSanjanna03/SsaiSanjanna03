3ee408761e9bd64f78ecd07d75b062922d628000
SsaiSanjanna <145184518+SsaiSanjanna03@users.noreply.github.com>
2023-09-17 00:38:45 -0500

    Add files via upload

---
+++ CourseInfo.java
@@ -0,0 +1,23 @@
+/** 
+ * Prints a description of the course. 
+ *
+ * Activity 1.
+ * @author Ssai Sanjanna Ganji - COMP 1210 - 006
+ * @version 23rd August 2022
+ */
+public class CourseInfo {
+/**
+   * Prints course information to std output.
+   * @param args Command line arguments (not used).
+   */
+   public static void main(String[] args) {
+     
+     // prints course description.
+      System.out.println("This course provides an introduction to Java and");
+      System.out.println("object-oriented programming.");
+      // prints more details of course description.
+      System.out.println();
+      System.out.println("The course also introduces the basics"
+         + " of software development.");
+   }
+}
---
+++ StudentInfo.java
@@ -0,0 +1,19 @@
+/**
+* Simple program to print StudentInfo.
+* 
+* Activity 1.
+* @author Ssai Sanjanna Ganji - COMP 1210-006
+* @version 08/23/22
+*/
+public class StudentInfo {
+/**
+* Prints StudentInfo.
+* @param args Command line arguments - not used.
+*/
+   public static void main(String[] args) {
+      System.out.println("Name: Ssai Sanjanna Ganji");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   None");
+   }
+
+}
