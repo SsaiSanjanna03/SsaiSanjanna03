44b15b57e34b5afda110f14ecaf482367f4d9ab2
SsaiSanjanna <145184518+SsaiSanjanna03@users.noreply.github.com>
2023-09-18 19:33:02 -0500

    Project 1

---
+++ JLetter.java
@@ -0,0 +1,27 @@
+/** 
+* Prints a large block J letter.
+*
+* Project_1.
+* @author Ssai Sanjanna Ganji - COMP 1210 - 006
+* @version 23rd August 2022
+*/
+public class JLetter
+{
+/**
+    * Prints a large block 'J' letter with characters 'J','A','V','A'. 
+    * @param args Command line arguments (not used).
+    */
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ MyLifeGoals.java
@@ -0,0 +1,34 @@
+/**
+* Simple program to print MyLifeGoals.
+* 
+* Project_1.
+* @author Ssai Sanjanna Ganji - COMP 1210-006
+* @version 08/26/22
+*/
+public class MyLifeGoals
+{
+/**
+   * Prints My Life Goals to output.
+   * @param args Command line arguments (not used).
+   */
+   public static void main(String[] args)
+   {
+      System.out.println("Ssai Sanjanna Ganji");
+      System.out.println();
+   // prints my short-term goal.
+      System.out.println("My short-term life goal is to become fluent"
+         + " with coding languages. As this is my first time learning a"
+         + " coding subject, I’m afraid whether if I’am going to get"
+            + " good grades or not. But I am looking forward to overcome"
+               + " my fear and achieve my goal.");
+   // prints my medium-term goal.
+      System.out.println("My medium-term life goal is to lead a software"
+         + " project in my upcoming academic years. To be specific, I want"
+         + " to work on a project related to cyber-security.");
+   // prints my long-term goal.
+      System.out.println("My long-term life goal is to build a new advanced"
+         + " software that protects our devices from being hacked. I want my"
+         + " software to be useful for people of any age group.");
+   }
+}
+    
